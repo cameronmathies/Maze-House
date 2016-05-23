87417894dd3d852fefc6b68c03d4acdc7df3e2c6
cameronmathies <cmathies@mr.preble.com>
2016-05-23 13:10:19 -0400

    Is light lightting me up?

---
+++ New Unity Project/Assets/Test/Light test/Light.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Light : MonoBehaviour {
+
+	void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("I'm blind!!!!!!");
+    }
+}
