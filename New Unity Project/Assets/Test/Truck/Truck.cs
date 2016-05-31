25128fed199edc4bb61e1ed748b4d8e9401d1b8e
cameronmathies <cmathies@mr.preble.com>
2016-05-31 13:15:28 -0400

    Truck

---
+++ New Unity Project/Assets/Test/Truck/Truck.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Truck : MonoBehaviour {
+    public AudioSource TruckSounds;
+    public ParticleSystem End;
+    // Use this for initialization
+    
+	
+
+}
---
+++ New Unity Project/Assets/Test/Truck/TruckStart.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TruckStart : MonoBehaviour {
+
+	void OnTriggerEnter()
+    {
+
+    }
+}
